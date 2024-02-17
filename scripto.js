e8d128427e7aebc88eb8737128f35ecf9d2cd322
ilvescovoangelo <153830657+ilvescovoangelo@users.noreply.github.com>
2024-02-17 21:24:46 +0100

    hsfgjdfuijreuigfdgikjfgirjgkdvgsdjrqeithj24k2349 efhrth5rghefg

--- scripto.js
+++ scripto.js
@@ -1,5 +1,7 @@
+var moveCount = 0;
+
 function moveButton(buttonId) {
-    if (buttonId === 'noButton') {
+    if (buttonId === 'noButton' && moveCount < 10) {
         var button = document.getElementById(buttonId);
         var newX = Math.random() * (window.innerWidth - button.clientWidth);
         var newY = Math.random() * (window.innerHeight - button.clientHeight);
@@ -7,5 +9,7 @@ function moveButton(buttonId) {
         button.style.position = "absolute";
         button.style.left = newX + "px";
         button.style.top = newY + "px";
+        
+        moveCount++;
     }
 }
